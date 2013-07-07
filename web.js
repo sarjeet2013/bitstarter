067bea85d40493ad0e0756fd5d489b93eac797bd
Sarjeet Singh <sarjeets@usc.edu>
2013-07-07 03:11:13 +0000

    modified web.js to read contents from index.html

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-fs.open('./index.html', 'r', function(err, fd) {
-	fs.fstat(fd, function(err, stats) {
-		var bufferSize = stats.size,
-		    buffer = new Buffer(bufferSize);
-		fs.read(fd, buffer);
+//fs.open('./index.html', 'r', function(err, fd) {
+//	fs.fstat(fd, function(err, stats) {
+//		var bufferSize = stats.size,
+		    buffer = new Buffer();
+//		fs.read(fd, buffer);
 		app.get('/', function(request, response) {
   			response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
 });
