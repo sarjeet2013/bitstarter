78437f567b40af6bfbc04d7e761738913024dd1f
Sarjeet Singh <sarjeets@usc.edu>
2013-07-07 02:05:31 +0000

    modified web.js to read data from index.html

--- web.js
+++ web.js
@@ -3,12 +3,11 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-fs.open('index.html', 'r', function(err, fd) {
+fs.open('~/bitstarter/index.html', 'r', function(err, fd) {
 	fs.fstat(fd, function(err, stats) {
 		var bufferSize = stats.size,
 		    buffer = new Buffer(bufferSize);
 		fs.read(fd, buffer);
-		console.log(buffer.toString('ascii'));
 		app.get('/', function(request, response) {
   			response.send(buffer);
 });
@@ -21,5 +20,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
